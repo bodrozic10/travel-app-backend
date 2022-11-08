bb7daac428ac1101dc68cadfef7908fe08cfc6bc
bodrozic10 <bodrozicnikola3@gmail.com>
2022-11-08 07:16:25 +0100

    connect user schema and accommodation sckema

--- src/routes/accommodationRoute.ts
+++ src/routes/accommodationRoute.ts
@@ -3,9 +3,10 @@ import {
   getAccommodations,
   createAccommodation,
 } from "../controllers/accommodationController";
+import { protect } from "../controllers/authController";
 
 const router = express.Router();
 
-router.route("/").get(getAccommodations).post(createAccommodation);
+router.route("/").get(getAccommodations).post(protect, createAccommodation);
 
 export default router;
